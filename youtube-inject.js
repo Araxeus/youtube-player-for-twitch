896cbaf7b62fcf8a090284ba10fb6cd1d98d49e6
Araxeus <78568641+Araxeus@users.noreply.github.com>
2026-02-26 00:52:31 +0200

    auto sync on `live` button click

--- youtube-inject.js
+++ youtube-inject.js
@@ -1,18 +1,23 @@
 (() => {
+    const $ = document.querySelector.bind(document);
     setTimeout(init, 50);
 
     function init() {
+        const api = $('#movie_player');
+
         const shouldForceQuality = new URLSearchParams(
             window.location.search,
         ).get('forcequality');
-
         if (shouldForceQuality === '1') {
-            setMaxQuality();
+            setMaxQuality(api);
         }
+
+        $('div.ytp-time-display.ytp-live').addEventListener('click', () => {
+            setTimeout(() => api.setPlaybackRate(99), 50);
+        });
     }
 
-    function setMaxQuality() {
-        const api = document.querySelector('#movie_player');
+    function setMaxQuality(api) {
         const maxQuality = api.getAvailableQualityLevels()[0];
         api.setPlaybackQualityRange(maxQuality);
         console.log('Set youtube quality to', maxQuality);
